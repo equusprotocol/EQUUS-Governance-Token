760b28890473cc42e20f681f06d940b396ae7f8d
Equus Protocol <admin@equus.dev>
2020-08-19 12:07:33 +0100

    Update eqgt.sol

--- eqgt.sol
+++ eqgt.sol
@@ -482,8 +482,7 @@ interface ERC1820Registry {
 
 /// Base client to interact with the registry.
 contract ERC1820Client {
-    //ERC1820Registry constant ERC1820REGISTRY = ERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
-    ERC1820Registry constant ERC1820REGISTRY = ERC1820Registry(0x76c688A55773331a07b4f132D967D5DEC6F92b29);
+    ERC1820Registry constant ERC1820REGISTRY = ERC1820Registry(0x1820a4B7618BdE71Dce8cdc73aAB6C95905faD24);
     
     function setInterfaceImplementation(string memory _interfaceLabel, address _implementation) internal {
         bytes32 interfaceHash = keccak256(abi.encodePacked(_interfaceLabel));
